3e20ea2c547b228134c2aea8c1db4de63652b15f
Raul Luis <soyrauluis@outlook.com>
2022-03-05 01:02:09 -0400

    Create models

---
+++ Entidades/ProductoDetalles.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+namespace ProductsBlazor.Entidades;
+
+public class ProductoDetalles
+{
+
+    [Key]
+    public int ProductoDetallesId { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "La presentacion no puede exceder {1} caracteres")]
+    [MinLength(3, ErrorMessage = "La presentacion al menos {1} caracteres")]
+    public string? Presentacion { get; set; }
+
+    [Required]
+    [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
+    public float? Cantidad { get; set; }
+
+    [Required]
+    [Range(0, float.MaxValue, ErrorMessage = "El precio debe ser mayor a {1} y menor a {2}")]
+    public float? Precio { get; set; }
+    public int ProductoId { get; set; }
+}
---
+++ Entidades/Productos.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductsBlazor.Entidades;
+public class Productos
+{
+    [Key]
+    public int ProductoId { get; set; }
+
+    [Required(ErrorMessage = "La descripcion es obligatoria")]
+    [MinLength(3, ErrorMessage = "La descripcion al menos {1} caracteres")]
+    [MaxLength(50, ErrorMessage = "La descripcion no puede exceder {1} caracteres")]
+    public string? Descripcion { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "El precio debe ser mayor a {1} y menor a {2}")]
+    public int Existencia { get; set; }
+
+    [Required]
+    [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
+    public float Costo { get; set; }
+
+    [Required]
+    [Range(0, float.MaxValue, ErrorMessage = "El costo debe ser mayor a {1} y menor a {2}")]
+    public float ValorInventario { get; set; }
+
+    [Required]
+    [Range(1, float.MaxValue, ErrorMessage = "El precio debe ser mayor a {1} y menor a {2}")]
+    public float Precio { get; set; }
+
+    [Required]
+    [Range(0.01, 100, ErrorMessage = "La ganancia debe ser mayor a {1} y menor a {2}")]
+    public float Ganancia { get; set; }
+
+    [ForeignKey("ProductoId")]
+    public virtual List<ProductoDetalles> ProductoDetalles { get; set; } = new List<ProductoDetalles>();
+
+}
