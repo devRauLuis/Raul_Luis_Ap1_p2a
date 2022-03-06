cbdf1adf942172229b775aa7e14d8b8e87fdf078
Raul Luis <soyrauluis@outlook.com>
2022-03-06 15:44:06 -0400

    Add ProductoDetallesEmpacados view

--- DAL/ProductsContext.cs
+++ DAL/ProductsContext.cs
@@ -7,4 +7,14 @@ public class ProductsContext : DbContext
     public ProductsContext(DbContextOptions<ProductsContext> options) : base(options) { }
     public DbSet<Productos> Productos { get; set; }
 
+    public DbSet<ProductoDetallesEmpacados> ProductoDetallesEmpacados { get; set; }
+
+    #region Required
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductoDetallesEmpacados>().ToView(nameof(ProductoDetallesEmpacados)).HasKey(p => p.ProductoDetallesId);
+
+    }
+    #endregion
+
 }
--- Migrations/20220306061445_ProductosDetallesEmpacadosView.Designer.cs
+++ Migrations/20220306061445_ProductosDetallesEmpacadosView.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductsBlazor.DAL;
 
@@ -10,9 +11,10 @@ using ProductsBlazor.DAL;
 namespace ProductsBlazor.Migrations
 {
     [DbContext(typeof(ProductsContext))]
-    partial class ProductsContextModelSnapshot : ModelSnapshot
+    [Migration("20220306061445_ProductosDetallesEmpacadosView")]
+    partial class ProductosDetallesEmpacadosView
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.2");
